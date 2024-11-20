6a9e7ba1d36281fc2dedb45e1a41e662d49f5d6b
Prashant Tiwari <prashanttiwari2585@gmail.com>
2024-11-20 18:49:51 +0530

    fullstack button

--- components/main/Navbar.tsx
+++ components/main/Navbar.tsx
@@ -3,58 +3,58 @@ import Image from 'next/image';
 import React from 'react';
 
 const Navbar = () => {
-  return (
-    <div className="w-full h-[65px] fixed top-0 shadow-lg shadow-[#2A0E61]/50 bg-[#03001417] backdrop-blur-md z-50 px-10">
-      <div className="w-full h-full flex flex-row items-center justify-between m-auto px-[10px]">
-        <a href="#about-me" className="flex items-center">
-          <span className="font-bold ml-[10px] hidden md:block transition-transform hover:scale-110 text-transparent bg-clip-text bg-gradient-to-r from-purple-500 to-cyan-500">
-            Prashant Tiwari
-          </span>
-        </a>
+    return (
+        <div className="w-full h-[65px] fixed top-0 shadow-lg shadow-[#2A0E61]/50 bg-[#03001417] backdrop-blur-md z-50 px-10">
+            <div className="w-full h-full flex flex-row items-center justify-between m-auto px-[10px]">
+                <a href="#about-me" className="flex items-center">
+                    <span className="font-bold ml-[10px] hidden md:block text-gray-300 transition-transform hover:scale-110 text-transparent bg-clip-text bg-gradient-to-r from-purple-500 to-cyan-500">
+                        Prashant Tiwari
+                    </span>
+                </a>
 
-        <div className="flex-grow flex justify-center mr-[70px]">
-          <div className="flex items-center justify-between border border-[#7042f861] bg-[#0300145e] px-[20px] py-[10px] rounded-full text-gray-200 max-w-[500px] w-full">
-            <a
-              href="#about-me"
-              className="cursor-pointer transition-all hover:underline hover:text-purple-400"
-            >
-              About me
-            </a>
-            <a
-              href="#skills"
-              className="cursor-pointer transition-all hover:underline hover:text-purple-400"
-            >
-              Skills
-            </a>
-            <a
-              href="#projects"
-              className="cursor-pointer transition-all hover:underline hover:text-purple-400"
-            >
-              Projects
-            </a>
-          </div>
-        </div>
+                <div className="flex-grow flex justify-center mr-[70px]">
+                    <div className="flex items-center justify-between border border-[#7042f861] bg-[#0300145e] px-[20px] py-[10px] rounded-full text-gray-200 max-w-[500px] w-full">
+                        <a
+                            href="#about-me"
+                            className="cursor-pointer transition-all hover:underline hover:text-purple-400"
+                        >
+                            About me
+                        </a>
+                        <a
+                            href="#skills"
+                            className="cursor-pointer transition-all hover:underline hover:text-purple-400"
+                        >
+                            Skills
+                        </a>
+                        <a
+                            href="#projects"
+                            className="cursor-pointer transition-all hover:underline hover:text-purple-400"
+                        >
+                            Projects
+                        </a>
+                    </div>
+                </div>
 
-        <div className="flex flex-row gap-5">
-          {Socials.map((social) => (
-            <a
-              href={social.link}
-              key={social.name}
-              className="transition-transform hover:scale-110"
-            >
-              <Image
-                src={social.src}
-                alt={social.name}
-                key={social.name}
-                width={24}
-                height={24}
-              />
-            </a>
-          ))}
+                <div className="flex flex-row gap-5">
+                    {Socials.map((social) => (
+                        <a
+                            href={social.link}
+                            key={social.name}
+                            className="transition-transform hover:scale-110"
+                        >
+                            <Image
+                                src={social.src}
+                                alt={social.name}
+                                key={social.name}
+                                width={24}
+                                height={24}
+                            />
+                        </a>
+                    ))}
+                </div>
+            </div>
         </div>
-      </div>
-    </div>
-  );
+    );
 };
 
 export default Navbar;
--- components/sub/Herocontent.tsx
+++ components/sub/Herocontent.tsx
@@ -13,32 +13,31 @@ const Herocontent = () => {
       animate="visible"
       className="flex flex-row items-center justify-between mt-40 px-20 w-full z-[20]"
     >
-      <div className="h-full w-1/2 flex flex-col gap-5 justify-center text-start">
-        <motion.div
-          variants={slideInFromTop}
-          className="Welcome-box py-[15px] px-[4px] border border-[#7042f88b] opacity-[0.9]"
-        >
-          <SparklesIcon className="text-[#b49bff] mr-[10px] h-5 w-5" />
-          <h1 className="Welcome-text text-[13px]">Full Stack Web Developer</h1>
-        </motion.div>
+      <div className="h-full w-1/2 flex flex-col gap-5 justify-center text-start ">
+
 
         <motion.div
           variants={slideInFromLeft(0.5)}
-          className="flex flex-col gap-6 mt-6 text-6xl font-bold text-white max-w-[600px]"
+          className="flex flex-col gap-6 mt-8 text-6xl font-bold text-white max-w-[600px]"
         >
           <span> Hi I&apos;m <br /> <span className="text-transparent bg-clip-text bg-gradient-to-r from-purple-500 to-cyan-500"> Prashant Tiwari <br /> </span>
-            Full Stack Web Developer
+            <motion.div
+              variants={slideInFromTop}
+              className="Welcome-box py-[15px] px-[8px] border mt-3 border-[#7042f88b] opacity-[0.9] "
+            >
+              <SparklesIcon className="text-[#b49bff] mr-[10px] h-5 w-5" />
+              <h1 className="Welcome-text text-[13px] ">Full Stack Web Developer</h1>
+            </motion.div>
           </span>
         </motion.div>
 
         <motion.p
           variants={slideInFromLeft(0.8)}
-          className="text-lg text-gray-400 my-5 max-w-[600px]"
+          className="text-lg text-gray-400 max-w-[600px] hover:animate-wave"
         >
-          I&apos;m a Full Stack Software Engineer skilled in building scalable web applications using MERN.
-          I’m also a passionate coder with 650+ problem solved on LeetCode and have led tech teams in robotics and college events.
-          Check out my projects and achievements!
+          I’m a 4th-year student at IIIT Surat, turning coffee into code and bugs into features. With 650+ LeetCode problems solved and a knack for building MERN stack applications, I promise I’m not just debugging my life—check out my projects and see for yourself!
         </motion.p>
+
 
         <motion.a
           variants={slideInFromLeft(1)}
