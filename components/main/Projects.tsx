372bbe9b69fca181d72c8536cd25ee70c6b951fa
Prashant Tiwari <prashanttiwari2585@gmail.com>
2025-02-08 11:41:51 +0530

    added project

--- components/main/Projects.tsx
+++ components/main/Projects.tsx
@@ -10,7 +10,7 @@ const Projects = () => {
       <h1 className="text-[40px] font-semibold text-transparent bg-clip-text bg-gradient-to-r from-purple-500 to-cyan-500 py-20">
         My Projects
       </h1>
-      <div className="h-full w-full flex flex-col md:flex-row gap-10 px-10 pl-[250px]">
+      <div className="h-full w-full grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-6 px-10 pl-[100px]">
         <a href="https://github.com/Pacificon/Cybercode">
           <ProjectCard
             src="/cybercodemain.jpg"
@@ -31,6 +31,13 @@ const Projects = () => {
             src="/portfolio.jpg"
             title="Personal Website Portfolio"
             description="A space-themed portfolio built with Next.js, featuring smooth animations and a cosmic design. Showcases my projects, skills, and experience in a captivating, responsive layout."
+          />
+        </a>
+        <a href="https://github.com/Pacificon/grocery_store">
+          <ProjectCard
+            src="/grocery_store.jpg"
+            title="Grocery Store App"
+            description="This application is a grocery store platform developed with Flask for the backend and a dynamic frontend using HTML, CSS, and JavaScript."
           />
         </a>
       </div>
